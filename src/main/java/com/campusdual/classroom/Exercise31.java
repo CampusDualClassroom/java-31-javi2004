355e177b13cab0360d968622fdfce12e67f3d139
Javi Pérez <javiperezotero10@gmail.com>
2024-10-01 10:41:46 +0200

    Ejercicio 31 acabado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,24 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
+    public static void devolvertxt (String rutaArchivo) {
+        try(BufferedReader br = new BufferedReader(new FileReader(rutaArchivo));) {
+            String linea;
+            while ((linea=br.readLine())!= null){
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Ocuriio un error: "+e.getMessage());
+        }
 
-    public static void main(String[] args) {
-
+    }
+    public static void main(String[] args)  {
+        String ruta = "src/main/resources/lorem.txt";
+        devolvertxt(ruta);
     }
 }
